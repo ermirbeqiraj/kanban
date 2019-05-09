02a1f6053161b16f9110377b939db2420d4c520c
florianbruci <florianbruci@gmail.com>
2019-05-09 14:25:41 +0200

    project crud

--- src/Issue.Web/ClientApp/src/app/mng-project/create-project.component.ts
+++ src/Issue.Web/ClientApp/src/app/mng-project/create-project.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from '../services/projects.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { InternalProjectModel } from '../models/ProjectModels';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-project',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-project.component.css']
 })
 export class CreateProjectComponent implements OnInit {
+  intProject: InternalProjectModel;
+  projectForm: FormGroup;
 
-  constructor() { }
+  constructor(private projectsService: ProjectsService, private formBuilder: FormBuilder, private router: Router) {
+    this.projectForm = formBuilder.group({
+      name: ["", [Validators.required, Validators.maxLength(50)]],
+      description: ["", Validators.maxLength(500)],
+      repositoryUrl: ["", Validators.maxLength(300)],
+      stagingUrl: ["", Validators.maxLength(300)]
+    });
+  }
 
   ngOnInit() {
   }
+  create() { }
 
+  goToList() {
+    this.router.navigate(['./list-project']);
+  }
+
+  submit({ value, valid }: { value: InternalProjectModel, valid: boolean }) {
+    if (valid) {
+      // continue
+      this.projectsService.create(value).subscribe(response => {
+        this.create();
+        if (valid)
+          this.router.navigate(['./list-project']);
+      });
+    }
+    else {
+      console.log("Error");
+    }
+  }
 }
--- src/Issue.Web/ClientApp/src/app/mng-project/update-project.component.ts
+++ src/Issue.Web/ClientApp/src/app/mng-project/update-project.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { InternalProjectModel } from '../models/ProjectModels';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProjectsService } from '../services/projects.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-update-project',
@@ -6,10 +10,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-project.component.css']
 })
 export class UpdateProjectComponent implements OnInit {
+  intProject: InternalProjectModel;
+  currentId?: number;
+  projectForm: FormGroup;
 
-  constructor() { }
+  constructor(private projectService: ProjectsService, private activatedRouter: ActivatedRoute, private formBuilder: FormBuilder, private route: Router) {
+    this.projectForm = formBuilder.group({
+      id: ['', Validators.required],
+      name: ['', [Validators.required, Validators.maxLength(50)]],
+      description: ['', Validators.maxLength(500)],
+      repositoryUrl: ['', Validators.maxLength(300)],
+      stagingUrl: ['', Validators.maxLength(300)]
+
+    });
+  }
 
   ngOnInit() {
+      this.activatedRouter.params.subscribe(params => {
+        if (params.id) {
+          this.currentId = parseInt(params.id);
+          this.getProject();
+        }
+        else {
+          console.log("Error");
+        }
+      });
+    
   }
+  goToList() {
+    this.route.navigate(['./list-project']);
+  }
+
+
+
+  getProject() {
+    if (this.currentId) {
+      this.projectService.getById(this.currentId).subscribe(response => {
+        this.projectForm.patchValue({
+          id: response.id,
+          name: response.name,
+          description: response.description,
+          repositoryUrl: response.repositoryUrl,
+          stagingUrl: response.stagingUrl
+        });
+      });
+    }
+    else
+      console.log("Nuk vjen Projekti");
+    
+  }
+
+  submit({ value, valid }: { value: InternalProjectModel, valid: boolean }) {
+    if (valid) {
+      this.projectService.update(value).subscribe(response => {
+        this.route.navigate(['./list-project']);
+      })
+    }
+    else {
+      console.log("ERROR");
+    }
+  }
+
 
 }
